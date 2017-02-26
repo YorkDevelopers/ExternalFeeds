0740bd630a6b74c01e22b58f6bb5c4638b74b078
DavidBetteridge <davidabetteridge@gmail.com>
2017-02-26 16:02:47 +0000

    Restricted meetings to within 25 miles

--- YorkDevelopers.Meetup/Models/Venue.cs
+++ YorkDevelopers.Meetup/Models/Venue.cs
@@ -3,5 +3,9 @@
     public class Venue
     {
         public string name { get; set; }
+
+        public double lat { get; set; }
+
+        public double lon { get; set; }
     }
 }
--- YorkDevelopers.Meetup/Program.cs
+++ YorkDevelopers.Meetup/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using YamlDotNet.Serialization;
 using YorkDevelopers.Meetup.Models;
@@ -9,42 +8,62 @@ using YorkDevelopers.Shared;
 
 namespace YorkDevelopers.Meetup
 {
+    /// <summary>
+    /// The application will download any suitable events from Meetup and then 
+    /// write them to a yaml file called Meetup.yml.  This file will then
+    /// be pushed into our git hub repro.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             const string URL = "https://api.meetup.com";
-            
             const string TECH = "292";
 
+            // Location of the Perky Peacock
+            const double LAT = 53.960636138916016;
+            const double LON = -1.0860970020294189;
+            const double LARGEST_DISTANCE = 25;
+
             var client = PrepareHttpClient(new Uri(URL));
-            var allEvents = new List<YorkDevelopers.Shared.Common>();
-
             var events = GET<List<Event>>(client, "/recommended/events?sign=true&key=" + Settings.Default.MEETUPTOKEN + "&fields=group_photo&topic_category=" + TECH);
+            var geoData = new GeoData();
 
+            var allEvents = new List<YorkDevelopers.Shared.Common>();
             foreach (var evt in events)
             {
-                var common = new Common();
-                common.Name = evt.name;
-                common.Description = evt.description;
-                common.URL = evt.link;
-                common.IsFree = ((evt.fee?.amount ?? 0) == 0);
-                common.Logo = evt.group.photo?.thumb_link;
-
-                common.Starts = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(evt.time));
-
-                if (evt.duration == null)
+                // Is this event near to us?
+                var distance = geoData.distance(LAT, LON, evt.venue.lat, evt.venue.lon, 'M');
+                if (distance <= LARGEST_DISTANCE)
                 {
-                    // If no duration is supplied,  then meetup assumes 3 hours
-                    common.Ends = common.Starts.AddHours(3);
+                    var common = new Common();
+                    common.Name = evt.name;
+                    common.Description = evt.description;
+                    common.URL = evt.link;
+
+                    // An event is free unless it contains a fee record with an amount which isn't 0
+                    common.IsFree = ((evt.fee?.amount ?? 0) == 0);
+
+                    // Events optionally have a group photo
+                    common.Logo = evt.group.photo?.thumb_link;
+
+                    // The start time is held in milliseconds
+                    common.Starts = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(evt.time));
+
+                    if (evt.duration == null)
+                    {
+                        // If no duration is supplied,  then meetup assumes 3 hours
+                        common.Ends = common.Starts.AddHours(3);
+                    }
+                    else
+                    {
+                        // Duration is also held in milliseconds
+                        var duration = TimeSpan.FromMilliseconds(double.Parse(evt.duration));
+                        common.Ends = common.Starts + duration;
+                    }
+                    common.Venue = evt.venue?.name;
+                    allEvents.Add(common);
                 }
-                else
-                {
-                    var duration = TimeSpan.FromMilliseconds(double.Parse(evt.duration));
-                    common.Ends = common.Starts + duration;
-                }
-                common.Venue = evt.venue?.name;
-                allEvents.Add(common);
             }
 
             var serializer = new Serializer();
