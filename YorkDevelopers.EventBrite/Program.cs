825919087bed2299c24a79f9a2e62601d1ee6462
DavidBetteridge <davidabetteridge@gmail.com>
2017-02-26 16:14:46 +0000

    Removed St Johns only events

--- YorkDevelopers.EventBrite/Program.cs
+++ YorkDevelopers.EventBrite/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
@@ -9,21 +10,35 @@ using YorkDevelopers.Shared;
 
 namespace YorkDevelopers.EventBrite
 {
+    /// <summary>
+    /// The application will download any suitable events from EventBrite and then 
+    /// write them to a yaml file called EventBrite.yml.  This file will then
+    /// be pushed into our git hub repro.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point - takes no arguments
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
         {
             const string CATEGORY = "102"; //Science and Tech
             const string URL = "https://www.eventbriteapi.com";
             const string FILENAME = "_data/EventBrite.yml";
 
+            // We keep a cache of venues so that we only have to fetch each one once.
             var venues = new Dictionary<string, Venue>();
+
+            // Use the EventBrite API to find all the Science and Tech events in York
             var client = PrepareHttpClient(new Uri(URL), Settings.Default.EVENTBRITE_TOKEN);
             var events = GET<Responses>(client, $"/v3/events/search/?location.address=York&categories={CATEGORY}");
 
+            // Take all the events and convert them into our 'Commmon' event format
             var allEvents = new List<Common>();
             foreach (var evt in events.events)
             {
+                // Either take the venue from our cache or go and fetch it
                 var venue = default(Venue);
                 if (!venues.TryGetValue(evt.venue_id, out venue))
                 {
@@ -31,6 +46,7 @@ namespace YorkDevelopers.EventBrite
                     venues.Add(evt.venue_id, venue);
                 }
 
+                // Populate our common structure
                 var common = new Common();
                 common.Name = evt.name.text;
                 common.Description = evt.description.text;
@@ -43,8 +59,9 @@ namespace YorkDevelopers.EventBrite
                 allEvents.Add(common);
             }
 
+            // Convert all the events into yaml
             var serializer = new Serializer();
-            var yaml = serializer.Serialize(allEvents);
+            var yaml = serializer.Serialize(allEvents.Where(IncludeEvent));
 
             // Push the file to git
             var gitHubClient = new GitHub();
@@ -52,6 +69,27 @@ namespace YorkDevelopers.EventBrite
 
         }
 
+        /// <summary>
+        /// Are we happy to include this event?
+        /// </summary>
+        /// <param name="common"></param>
+        /// <returns></returns>
+        private static bool IncludeEvent(Common common)
+        {
+            if (common.Description.Contains("These courses are for staff and students of York St. John University only"))
+                return false;
+            else
+                return true;
+        }
+
+        /// <summary>
+        /// The image URL we are provided is a bit strange as it starts with a page and
+        /// then takes the actual image page as a querystring parameter
+        /// We remove the initial page,  and then reverse the URL encoding on the rest
+        /// of the URL.
+        /// </summary>
+        /// <param name="originalURL"></param>
+        /// <returns></returns>
         private static string CleanUpURL(string originalURL)
         {
             const string PREFIX = "https://img.evbuc.com/";
@@ -65,6 +103,13 @@ namespace YorkDevelopers.EventBrite
             return originalURL;
         }
 
+        /// <summary>
+        /// When calling the EventBrite API we have to pass our App's token
+        /// in the header.
+        /// </summary>
+        /// <param name="endPoint"></param>
+        /// <param name="oAuthtoken"></param>
+        /// <returns></returns>
         private static HttpClient PrepareHttpClient(Uri endPoint, string oAuthtoken)
         {
             var client = new HttpClient();
@@ -73,6 +118,13 @@ namespace YorkDevelopers.EventBrite
             return client;
         }
 
+        /// <summary>
+        /// Wrapper around an HTTP GET call
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="client"></param>
+        /// <param name="apiCall"></param>
+        /// <returns></returns>
         public static T GET<T>(HttpClient client, string apiCall)
         {
             // Proxy the call onto our service.
